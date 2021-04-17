f23f655603665d6a3c58f97628371899c5e786bd
AnatolyKaz <ankaz.work@gmail.com>
2021-04-17 15:52:15 +0500

    Update smo-3.js

--- smo-3.js
+++ smo-3.js
@@ -1,249 +1,244 @@
-imitation()
 
 //==============imitation=================
 function imitation() {
-    const time = 36000 //с
-    const lambda = 0.05
-    const mu = 0.005
-    const eta = 0.01
-    const n = 4
-    const l = 2
-    const m = 1
-    const timeInQueue = 150 // время нахождения заявки в очереди сек
+	const time = 36000 //с
+	const lambda = 0.05
+	const mu = 0.005
+	const eta = 0.01
+	const n = 3
+	const l = 2
+	const m = 1
+	const timeInQueue = 150 // время нахождения заявки в очереди сек
 
-    let freeChannels = n
-    let countApps = [] //обслуженные заявки
-    let allApps = 0 /// все заявки
-    let timeNewApp = 0 //время прихода новой заявки
-    let refusalAppsCount = 0 // количество заявок с отказом
-    let freeTimeCount = 0 //время простоя системы
-    let apps = [] //массив заявок на обслуживании
-    let queue = [] // заявки в очереди
+	let freeChannels = n
+	let countApps = [] //обслуженные заявки
+	let allApps = 0 /// все заявки
+	let timeNewApp = 0 //время прихода новой заявки
+	let refusalAppsCount = 0 // количество заявок с отказом
+	let freeTimeCount = 0 //время простоя системы
+	let apps = [] //массив заявок на обслуживании
+	let queue = [] // заявки в очереди
 
-    /// параметры которые необходимо найти
-    let countChannels = [] // количество занятых каналов в оазные моменты времени
-    let fullWorkingSystem = 0 // количество моментов времени когда истема полностью занята
-    let queueTime = [] // время проведенное в очереди
+	/// параметры которые необходимо найти
+	let countChannels = [] // количество занятых каналов в оазные моменты времени
+	let fullWorkingSystem = 0 // количество моментов времени когда истема полностью занята
+	let queueTime = [] // время проведенное в очереди
 
-    ////=======main cycle==================
+	////=======main cycle==================
 
-    for (let i = 0; i <= time; i++) {
+	for (let i = 0; i <= time; i++) {
+		apps = apps.length // проверка готовнисти заявки
+			? apps.filter((app) => {
+					if (app.serviceTime + app.arrivalTimeApp === i) {
+						freeChannels += app.workingChannels
+						countApps.push(app)
+						return 0
+					} else {
+						return 1
+					}
+			  })
+			: apps
 
-        apps = apps.length // проверка готовнисти заявки
-            ? apps.filter(app => {
-                if (app.serviceTime + app.arrivalTimeApp === i) {
-                    freeChannels += app.workingChannels
-                    countApps.push(app)
-                    return 0
-                } else {
-                    return 1
-                }
-            })
-            : apps
+		queue = queue.length
+			? queue.filter((app) => {
+					if (app.arrivalTimeApp + timeInQueue === i) {
+						queueTime.push(timeInQueue)
+						refusalAppsCount++
+						return 0
+					} else {
+						return 1
+					}
+			  })
+			: queue
 
-        queue = queue.length
-            ? queue.filter(app => {
-                if (app.arrivalTimeApp + timeInQueue === i) {
-                    queueTime.push(timeInQueue)
-                    refusalAppsCount++
-                    return 0
-                } else {
-                    return 1
-                }
-            }) : queue
+		if (freeChannels) {
+			let systemState = redistributionChannels(apps, queue, i)
+			apps = systemState.apps
+			queue = systemState.queue
+		}
 
-        if (freeChannels) {
-            let systemState = redistributionChannels(apps, queue, i)
-            apps = systemState.apps
-            queue = systemState.queue
-        }
+		timeNewApp = i ? timeNewApp : getTimeNewApp(i)
 
-        timeNewApp = i ? timeNewApp : getTimeNewApp(i)
+		if (i === timeNewApp) {
+			allApps++
+			timeNewApp = getTimeNewApp(i)
 
-        if (i === timeNewApp) {
+			if (apps.length === n && queue.length === m) {
+				refusalAppsCount++
+			} else if (apps.length === n && queue.length < m) {
+				queue.push({
+					arrivalTimeApp: i,
+				})
+			} else {
+				let newApp = {
+					serviceTime: getServiceTime(l),
+					arrivalTimeApp: i,
+					workingChannels: l,
+				}
 
-            allApps++
-            timeNewApp = getTimeNewApp(i)
+				if (!apps.length && l <= n) {
+					freeChannels -= l
+					apps.push(newApp)
+					continue
+				}
 
-            if (apps.length === n && queue.length === m) {
-                refusalAppsCount++
-            } else if (apps.length === n && queue.length < m) {
+				if (n >= 2 * l && apps.length === 1) {
+					freeChannels -= l
+					apps.push(newApp)
+					continue
+				}
 
-                queue.push({
-                    arrivalTimeApp: i,
-                })
-            } else {
-                let newApp = {
-                    serviceTime: getServiceTime(l),
-                    arrivalTimeApp: i,
-                    workingChannels: l,
-                }
+				if ((apps.length + 1) * l <= n) {
+					freeChannels -= l
+					apps.push(newApp)
+					continue
+				}
 
-                if (!apps.length && l <= n) {
-                    freeChannels -= l
-                    apps.push(newApp)
-                    continue
-                }
+				if ((apps.length + 1) * l > n && apps.length < n) {
+					if (apps.length > 1) {
+						apps = sortByWorkingChannels(apps)
 
-                if (n >= 2 * l && apps.length === 1) {
-                    freeChannels -= l
-                    apps.push(newApp)
-                    continue
-                }
+						if (apps[0].workingChannels > 1) {
+							let recalcChannels = recalcServiceTimeApp(
+								apps[0],
+								newApp,
+								i
+							)
+							apps[0] = recalcChannels.currentApp
+							newApp = recalcChannels.newApp
+							apps.push(newApp)
+							continue
+						}
+					} else if (l === n && apps.length === 1) {
+						let recalcChannels = recalcServiceTimeApp(
+							apps[0],
+							newApp,
+							i
+						)
+						apps[0] = recalcChannels.currentApp
+						newApp = recalcChannels.newApp
+						apps.push(newApp)
+					} else {
+						newApp.serviceTime = getServiceTime(freeChannels)
+						newApp.workingChannels = freeChannels
+						freeChannels = 0
+						apps.push(newApp)
+					}
+				}
+			}
+		}
 
-                if ((apps.length + 1) * l <= n) {
-                    freeChannels -= l
-                    apps.push(newApp)
-                    continue
-                }
+		countChannels.push(apps.length)
 
-                if ((apps.length + 1) * l > n && apps.length < n) {
-                    if (apps.length > 1) {
-                        apps = sortByWorkingChannels(apps)
+		if (apps.length === n && queue.length === m) {
+			fullWorkingSystem++
+		}
+		if (!apps.length && i) {
+			freeTimeCount++
+		}
+	}
+	////=======main cycle==================
+	/////=============calc params=================
+	let t = queueTime.length
+		? queueTime.reduce((a, b) => a + b) / queueTime.length
+		: 0
 
-                        if (apps[0].workingChannels > 1) {
-                            let recalcChannels = recalcServiceTimeApp(
-                                apps[0],
-                                newApp,
-                                i
-                            )
-                            apps[0] = recalcChannels.currentApp
-                            newApp = recalcChannels.newApp
-                            apps.push(newApp)
-                            continue
-                        }
-                    } else if (l === n && apps.length === 1) {
-                        let recalcChannels = recalcServiceTimeApp(
-                            apps[0],
-                            newApp,
-                            i
-                        )
-                        apps[0] = recalcChannels.currentApp
-                        newApp = recalcChannels.newApp
-                        apps.push(newApp)
-                    } else {
-                        newApp.serviceTime = getServiceTime(freeChannels)
-                        newApp.workingChannels = freeChannels
-                        freeChannels = 0
-                        apps.push(newApp)
-                    }
-                }
-            }
-        }
-
-        countChannels.push(apps.length)
-
-        if (apps.length === n && queue.length === m) {
-            fullWorkingSystem++
-        }
-        if (!apps.length && i) {
-            freeTimeCount++
-        }
-    }
-    ////=======main cycle==================
-    /////=============calc params=================
-
-    console.log('\n')
-    console.log('Среднее число занятых каналов' + ' ' + (countChannels.reduce((a, b) => (a + b))) / countChannels.length)
-    console.log(
-		'Среднее время в очереди(сек)' +
+	console.log('\n')
+	console.log(
+		'Среднее число занятых каналов' +
 			' ' +
-			queueTime.reduce((a, b) => a + b) / queueTime.length
+			countChannels.reduce((a, b) => a + b) / countChannels.length
 	)
-    console.log(
+	console.log('Среднее время в очереди(сек)' + ' ' + t)
+	console.log(
 		'Вероятность полной занятости ситемы' +
 			' ' +
 			fullWorkingSystem / (time - freeTimeCount)
 	)
-    console.log('\n')
+	console.log('\n')
 
-    /////=============calc params=================
-    //////==========functions=====================
-    function redistributionChannels(apps, queue, i) {
-        if (queue.length) {
-            for (let j = 0; j < queue.length; j++) {
-                if (freeChannels) {
-                    queue[j].serviceTime = getServiceTime(1)
-                    queue[j].workingChannels = 1
-                    freeChannels -= 1
-                    apps.push(queue[j])
-                    queueTime.push(i - queue[j].arrivalTimeApp)
-                    queue = queue.filter((app, index) => (index === j ? 0 : 1))
-                    apps = redistribution(apps, i)
-                }
-            }
+	/////=============calc params=================
+	//////==========functions=====================
+	function redistributionChannels(apps, queue, i) {
+		if (queue.length) {
+			for (let j = 0; j < queue.length; j++) {
+				if (freeChannels) {
+					queue[j].serviceTime = getServiceTime(1)
+					queue[j].workingChannels = 1
+					freeChannels -= 1
+					apps.push(queue[j])
+					queueTime.push(i - queue[j].arrivalTimeApp)
+					queue = queue.filter((app, index) => (index === j ? 0 : 1))
+					apps = redistribution(apps, i)
+				}
+			}
+		} else if (apps.length) {
+			apps = redistribution(apps, i)
+		}
 
-        } else if (apps.length) {
-            apps = redistribution(apps, i)
-        }
+		return { apps, queue }
+	}
 
-        return {apps, queue}
-    }
+	function redistribution(apps, i) {
+		for (let j = 0; j < apps.length; j++) {
+			while (freeChannels) {
+				if (apps[j].workingChannels < l) {
+					apps[j].workingChannels += 1
+					freeChannels -= 1
+					apps[j].serviceTime =
+						i -
+						apps[j].arrivalTimeApp +
+						getServiceTime(apps[j].workingChannels)
+				} else {
+					break
+				}
+			}
+		}
+		return apps
+	}
 
-    function redistribution(apps, i) {
-        for (let j = 0; j < apps.length; j++) {
-            while (freeChannels) {
-                if (apps[j].workingChannels < l) {
-                    apps[j].workingChannels += 1
-                    freeChannels -= 1
-                    apps[j].serviceTime =
-                        i -
-                        apps[j].arrivalTimeApp +
-                        getServiceTime(apps[j].workingChannels)
-                } else {
-                    break
-                }
-            }
-        }
-        return apps
-    }
+	function sortByWorkingChannels(apps) {
+		apps.sort((prev, next) => {
+			if (prev.workingChannels < next.workingChannels) {
+				return 1
+			}
+			if (prev.workingChannels > next.workingChannels) {
+				return -1
+			}
+			return 0
+		})
 
-    function sortByWorkingChannels(apps) {
-        apps.sort((prev, next) => {
-            if (prev.workingChannels < next.workingChannels) {
-                return 1
-            }
-            if (prev.workingChannels > next.workingChannels) {
-                return -1
-            }
-            return 0
-        })
+		return apps
+	}
 
-        return apps
-    }
+	function recalcServiceTimeApp(currentApp, newApp, i) {
+		currentApp = recalcServiceTime(currentApp, i)
+		newApp.serviceTime = getServiceTime(1)
+		newApp.workingChannels = 1
 
-    function recalcServiceTimeApp(currentApp, newApp, i) {
-        currentApp = recalcServiceTime(currentApp, i)
-        newApp.serviceTime = getServiceTime(1)
-        newApp.workingChannels = 1
+		return { currentApp, newApp }
+	}
 
-        return {currentApp, newApp}
-    }
+	function recalcServiceTime(app, i) {
+		app.workingChannels -= 1
+		app.serviceTime =
+			i - app.arrivalTimeApp + getServiceTime(app.workingChannels)
 
-    function recalcServiceTime(app, i) {
-        app.workingChannels -= 1
-        app.serviceTime =
-            i - app.arrivalTimeApp + getServiceTime(app.workingChannels)
+		return app
+	}
 
-        return app
-    }
+	function getTimeNewApp(i) {
+		return i + Math.round((-1 / lambda) * Math.log(getRandom()))
+	}
 
-    function getTimeNewApp(i) {
-        return i + Math.round((-1 / lambda) * Math.log(getRandom()))
-    }
+	function getServiceTime(l) {
+		return Math.round((-1 / (l * mu + eta)) * Math.log(getRandom()))
+	}
 
-    function getServiceTime(l) {
-        return Math.round((-1 / (l * mu + eta)) * Math.log(getRandom()))
-    }
-
-
-    function getRandom() {
-        let min = 0.0000000001
-        let max = 0.9999999999
-        return Math.random() * (max - min) + min
-    }
+	function getRandom() {
+		let min = 0.0000000001
+		let max = 0.9
+		return Math.random() * (max - min) + min
+	}
 }
-
 //==============imitation=================
-
-
